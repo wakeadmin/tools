6549a63b0750432d2cd2242819df49bd968138bc
黎贵奇 <carney520@hotmail.com>
2022-07-12 14:26:41 +0800

    chore: 发布命令
    Change-Id: Iddf6a5e694882f9c459b830ba2674ce4468b0de8

--- packages/bay/scripts/publish-docker.js
+++ packages/bay/scripts/publish-docker.js
@@ -4,9 +4,12 @@ const pkg = require('../package.json');
 const server = process.env.DOCKER_SERVER;
 const user = process.env.DOCKER_USER;
 const password = process.env.DOCKER_PASSWORD;
+const skipLogin = process.env.DOCKER_SKIP_LOGIN;
 const image = `wkfe/bay`;
 
-ch.execSync(`docker login ${server || ''} -u ${user} -p '${password}'`, { stdio: 'inherit' });
+if (!skipLogin) {
+  ch.execSync(`docker login ${server || ''} -u ${user} -p '${password}'`, { stdio: 'inherit' });
+}
 
 // 设置版本
 ch.execSync(`docker tag ${image} ${image}:${pkg.version}`, { stdio: 'inherit' });
