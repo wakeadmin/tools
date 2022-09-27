9fc4470003a5da8aa1c10153707b1e17e200d95c
谭鹏 <tanpeng@wakedata.com>
2022-09-27 10:54:00 +0800

    feat(bay): 现在allows方法允许传入字符串
    Change-Id: I390cc522d9c589b95ede35ba57413989911d8052

--- packages/bay/src/services/permissions.ts
+++ packages/bay/src/services/permissions.ts
@@ -12,7 +12,8 @@ export type PermissionCheckType = 'AND' | 'OR';
  * @param to 权限标识符，在当前激活上下文中查找
  * @param type 检查的类型, 支持 AND 或 OR，在传递多个权限标识符时有用，AND 表示必须拥有全部权限，OR 表示拥有其中一个即可
  */
-export async function allows(to: string[], type: PermissionCheckType = 'OR') {
+export async function allows(to: string[] | string, type: PermissionCheckType = 'OR') {
+  const roles = Array.isArray(to) ? to : [to];
   const bayModel = getInject('DI.bay.BayModel');
 
   await bayModel.waitSetup();
@@ -22,9 +23,9 @@ export async function allows(to: string[], type: PermissionCheckType = 'OR') {
   };
 
   if (type === 'AND') {
-    return to.every(check);
+    return roles.every(check);
   } else {
-    return to.some(check);
+    return roles.some(check);
   }
 }
 
@@ -38,7 +39,8 @@ export async function allows(to: string[], type: PermissionCheckType = 'OR') {
  * @param to 权限标识符，在当前激活上下文中查找
  * @param type 检查的类型, 支持 AND 或 OR，在传递多个权限标识符时有用，AND 表示必须拥有全部权限，OR 表示拥有其中一个即可
  */
-export function allowsSync(to: string[], type: PermissionCheckType = 'OR') {
+export function allowsSync(to: string[] | string, type: PermissionCheckType = 'OR') {
+  const roles = Array.isArray(to) ? to : [to];
   const bayModel = getInject('DI.bay.BayModel');
   if (bayModel.status !== BayStatus.READY) {
     return false;
@@ -49,8 +51,8 @@ export function allowsSync(to: string[], type: PermissionCheckType = 'OR') {
   };
 
   if (type === 'AND') {
-    return to.every(check);
+    return roles.every(check);
   } else {
-    return to.some(check);
+    return roles.some(check);
   }
 }
