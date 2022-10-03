77b048d38154d77f1e6151e5b86ad4512795dd4d
黎贵奇 <carney520@hotmail.com>
2022-10-04 07:18:08 +0800

    revert(demi):回退
    Change-Id: I085fa2fc803a26e6add4f4028dfbe83272d00274

--- packages/demi/lib/index.mjs
+++ packages/demi/lib/index.mjs
@@ -1,14 +1,1 @@
-// FIX: 避免 vue 2 警告
-export const Fragment = undefined;
-export const Suspense = undefined;
-export const Teleport = undefined;
-export const KeepAlive = undefined;
-export const TransitionGroup = undefined;
-export const Transition = undefined;
-export const isVNode = undefined;
-export const withDirectives = undefined;
-export const vShow = undefined;
-export const resolveDirective = undefined;
-export const resolveComponent = undefined;
-
 export * from 'vue-demi';
