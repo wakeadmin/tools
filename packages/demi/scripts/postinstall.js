8359922303b9cddeadffa5577d4bf26b6399912c
黎贵奇 <carney520@hotmail.com>
2022-10-15 05:23:01 +0800

    feat(demi): 不支持 vue 2.7 以下版本
    Change-Id: I38075b88d05ec2f1d4dd6c2bf7cd0cf39b1db39c

--- packages/demi/scripts/postinstall.js
+++ packages/demi/scripts/postinstall.js
@@ -3,13 +3,15 @@ const { switchVersion, loadModule } = require('./utils');
 const Vue = loadModule('vue');
 
 if (!Vue || typeof Vue.version !== 'string') {
-  console.warn('[vue-demi] Vue is not found. Please run "npm install vue" to install.');
+  console.warn('[@wakeadmin/demi] Vue is not found. Please run "npm install vue" to install.');
+  process.exit(1);
 } else if (Vue.version.startsWith('2.7.')) {
   switchVersion(2.7);
 } else if (Vue.version.startsWith('2.')) {
-  switchVersion(2);
+  console.warn('[@wakeadmin/demi] Vue 2 不受支持，请升级或安装 2.7+');
+  process.exit(1);
 } else if (Vue.version.startsWith('3.')) {
   switchVersion(3);
 } else {
-  console.warn(`[vue-demi] Vue version v${Vue.version} is not suppported.`);
+  console.warn(`[@wakeadmin/demi] Vue version v${Vue.version} is not suppported.`);
 }
