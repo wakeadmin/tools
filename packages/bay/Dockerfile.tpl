59b413823d744b5dfc9dcd40c9f41bcb5f7238cc
黎贵奇 <carney520@hotmail.com>
2022-10-18 16:22:04 +0800

    feat(bay): 主题包本地备份
    Change-Id: I1eb8e76cdba69e2dbba76b12dc3a012c2843879f

--- packages/bay/Dockerfile.tpl
+++ packages/bay/Dockerfile.tpl
@@ -3,10 +3,13 @@ FROM <%=HOST_PREFIX%>wkfe/mapp
 WORKDIR /data
 COPY ./dist /data/source/__entry__
 
+# 主题本地备份，避免 npm 拉取失败
+COPY ./theme /data/source/__theme__
+
 # 基座版本号
 ENV MAPP_VERSION=<%=VERSION%>
 
-# 默认主题包
+# 默认主题包, 如果你想远程来取主题包，需要配置这一行
 ENV MAPP_NPM_THEME=<%=THEME%>
 
 <% if (MOCK_ENABLED) { %>
--- packages/bay/scripts/build-docker.js
+++ packages/bay/scripts/build-docker.js
@@ -2,11 +2,22 @@
  * 容器构建
  */
 // @ts-check
+const path = require('path');
+const fs = require('fs-extra');
+
 const build = require('../../../scripts/shared-docker-build');
-
 const pkg = require('../package.json');
-
 const { IMAGE_NAME, PLAYGROUND_IMAGE_NAME } = require('./shared');
+
+const THEME = pkg.theme;
+
+// copy theme
+const themeDir = path.join(path.dirname(require.resolve(path.posix.join(THEME, 'package.json'))), 'dist');
+const themeTargetDir = path.join(__dirname, '../theme');
+
+fs.ensureDirSync(themeTargetDir);
+fs.emptyDirSync(themeTargetDir);
+fs.copySync(themeDir, themeTargetDir);
 
 build(IMAGE_NAME, { VERSION: pkg.version, THEME: pkg.theme, MOCK_ENABLED: false }, '--pull');
 build(PLAYGROUND_IMAGE_NAME, { VERSION: pkg.version, THEME: pkg.theme, MOCK_ENABLED: true });
