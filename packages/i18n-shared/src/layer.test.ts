b6827d8b1f73234b2b81a40be91c290de00c4118
黎贵奇 <carney520@hotmail.com>
2023-07-03 18:54:56 +0800

    fix: 简化 layer 的实现
    Change-Id: Ieb9e7e0e0d38599d9467f105f8eb183b48d928c0

--- packages/i18n-shared/src/layer.test.ts
+++ packages/i18n-shared/src/layer.test.ts
@@ -3,34 +3,43 @@ import { LayerLink } from './layer';
 describe('LayerLink', () => {
   test('正常存取值', () => {
     const link = new LayerLink();
-    const value = link.value;
+    let value = link.flattenLayer() as any;
 
     // 初始层
     link.assignLayer(0, { one: '1' });
 
+    value = link.flattenLayer();
     expect(value.one).toBe('1');
 
     // 第1层
     link.assignLayer(1, { two: '2' });
+
+    value = link.flattenLayer();
     expect(value.two).toBe('2');
 
     // 第5层
     link.assignLayer(5, { five: '5' });
+    value = link.flattenLayer();
     expect(value.five).toBe('5');
 
     // 中间插入第3层
     link.assignLayer(3, { three: '3' });
+    value = link.flattenLayer();
     expect(value.three).toBe('3');
     expect(value.five).toBe('5');
 
     // 层数越低，查找优先级越高
     link.assignLayer(3, { five: '-5' });
+    value = link.flattenLayer();
     expect(value.five).toBe('-5');
 
     // 插入第6层
     link.assignLayer(6, { six: '6' });
+    value = link.flattenLayer();
     expect(value.six).toBe('6');
+
     link.assignLayer(5, { six: '-6' });
+    value = link.flattenLayer();
     expect(value.six).toBe('-6');
 
     expect(link.flattenLayer()).toEqual({
@@ -48,8 +57,15 @@ describe('LayerLink', () => {
     link.assignLayer(0, { foo: { bar: '1', baz: '2' } });
     link.assignLayer(0, { foo: { bar: '2' } });
     link.assignLayer(1, { foo: { bazz: '3' } });
+    link.assignLayer(1, { bay: { header: 'wk', nested: {} } });
+    link.assignLayer(0, { bay: { footer: 'wk', header: 'yk' } });
 
     expect(link.flattenLayer()).toEqual({
+      bay: {
+        footer: 'wk',
+        header: 'yk',
+        nested: {},
+      },
       foo: {
         bar: '2',
         baz: '2',
